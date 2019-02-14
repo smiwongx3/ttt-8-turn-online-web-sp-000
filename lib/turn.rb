d0f7e7d0d074275f5fb910b61a053356784745ce
Samantha Wong <smiwong920@gmail.com>
2019-02-14 22:09:37 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -19,3 +19,14 @@ def valid_move?(board, index)
   position.to_i.between?(0,8) && !position_taken?(board, position.to_i-1)
 end 
 
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
